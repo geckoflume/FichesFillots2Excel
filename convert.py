31d8ce6dbafaec4a0b03b9d642539f2795f20d4c
Florian Mornet <Florian.Mornet@bordeaux-inp.fr>
2019-09-08 16:25:55 +0200

    Changed code to split each form in a separate dir

--- convert.py
+++ convert.py
@@ -1,24 +1,28 @@
 import os
 from shutil import rmtree
-from pdf2image import convert_from_path
+import pdf2image
 
 form_dir = 'formulaires/'
 temp_dir = 'temp/'
 dpi = 250
 
-def create_temp(name):
-    if not os.path.exists(temp_dir):
-        os.mkdir(temp_dir)
-    rmtree(temp_dir + name)
-    os.mkdir(temp_dir + name)
+rmtree(temp_dir)
+
+def create_temp(name, count):
+    os.makedirs(temp_dir + name + '/' + str(count), mode=0o777, exist_ok=True)
 
 for file in os.listdir(form_dir):
     filename, file_extension = os.path.splitext(file)
     actual = filename.replace(' ', '-')
-    print('Current form: ' + file + '...')
-    create_temp(actual)
-    pages = convert_from_path(form_dir + file, dpi, fmt='jpeg', output_folder=temp_dir + actual)
+    nbForms = pdf2image._page_count(form_dir + file)
+    print('Current file: ' + file + ' ('+ str(int(nbForms / 2)) + ' forms)...')
+    count = 0
+    for page in range(1, nbForms, 2) :
+        create_temp(actual, count)
+        pdf2image.convert_from_path(form_dir + file, dpi, first_page=page, last_page = min(page + 2 - 1, nbForms), fmt='jpeg', output_folder=temp_dir + actual+ '/' + str(count))
+        count += 1
     print('Done!')
+
 
 
 #for page in pages:
