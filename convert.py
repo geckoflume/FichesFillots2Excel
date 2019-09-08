763f5759ec8e4038ed16f2546cdef91fe0ff2a2d
Florian Mornet <Florian.Mornet@bordeaux-inp.fr>
2019-09-08 17:02:36 +0200

    Forms separation now faster

--- convert.py
+++ convert.py
@@ -6,23 +6,36 @@ form_dir = 'formulaires/'
 temp_dir = 'temp/'
 dpi = 250
 
-rmtree(temp_dir)
+if os.path.exists(temp_dir):
+    rmtree(temp_dir, ign)
 
-def create_temp(name, count):
-    os.makedirs(temp_dir + name + '/' + str(count), mode=0o777, exist_ok=True)
+def create_temp(name):
+    os.makedirs(temp_dir + name, mode=0o777, exist_ok=True)
 
-for file in os.listdir(form_dir):
-    filename, file_extension = os.path.splitext(file)
-    actual = filename.replace(' ', '-')
-    nbForms = pdf2image._page_count(form_dir + file)
-    print('Current file: ' + file + ' ('+ str(int(nbForms / 2)) + ' forms)...')
-    count = 0
-    for page in range(1, nbForms, 2) :
-        create_temp(actual, count)
-        pdf2image.convert_from_path(form_dir + file, dpi, first_page=page, last_page = min(page + 2 - 1, nbForms), fmt='jpeg', output_folder=temp_dir + actual+ '/' + str(count))
-        count += 1
-    print('Done!')
+def extract_images():
+    for file in os.listdir(form_dir):
+        filename, file_extension = os.path.splitext(file)
+        actual = filename.replace(' ', '-')
+        create_temp(actual)
+        print('Current file: ' + file + '...')
+        pdf2image.convert_from_path(form_dir + file, dpi, fmt='jpeg', output_folder=temp_dir + actual)
+        print('Done!')
 
+def separate_forms():
+    for file in os.listdir(form_dir):
+        filename, file_extension = os.path.splitext(file)
+        actual = filename.replace(' ', '-')
+        count = 0
+        pics = os.listdir(temp_dir + actual)
+        for pic in range(0, len(pics), 2):
+            actual_form = actual + '/' + str(count)
+            create_temp(actual_form)
+            os.rename(temp_dir + actual + '/' + pics[pic], temp_dir + actual_form + '/' + pics[pic])
+            os.rename(temp_dir + actual + '/' + pics[pic + 1], temp_dir + actual_form + '/' + pics[pic + 1])
+            count += 1
+
+extract_images()
+separate_forms()
 
 
 #for page in pages:
